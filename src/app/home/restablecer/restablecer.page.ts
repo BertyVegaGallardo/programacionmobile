6cf0dd794187fec28d5ba0abf2c7ee6b38926995
BertyVegaGallardo <81119247+BertyVegaGallardo@users.noreply.github.com>
2021-09-25 00:31:32 -0300

    se agrega contenido a la rama

--- src/app/home/restablecer/restablecer.page.ts
+++ src/app/home/restablecer/restablecer.page.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UsuarioService}  from '../usuario.service';
+import { Usuario } from '../usuario.model';
+import { Router, NavigationExtras } from '@angular/router';
 @Component({
   selector: 'app-restablecer',
   templateUrl: './restablecer.page.html',
   styleUrls: ['./restablecer.page.scss'],
 })
 export class RestablecerPage implements OnInit {
+  user={
+    usuario:''
+    
+  };
+   usuarioServiceS: Usuario;
+     //variable para mostrar el campo faltante
+   campo: string;
 
-  constructor() { }
+   constructor(private router: Router,
+    private usuarioService: UsuarioService) { } // Se debe instanciar
+ 
 
   ngOnInit() {
   }
